d62e9a567187446187ac0f075ea70227688fd1f5
Carlos F P Rocha <carlosfprocha@gmail.com>
2026-06-23 21:49:30 -0300

    23062026

---
+++ src/main/java/Swing/dao/TarefaDAO.java
@@ -0,0 +1,10 @@
+package Swing.dao;
+
+import Swing.model.Tarefa;
+
+import java.util.List;
+
+public interface TarefaDAO {
+    void salvar(String descricao);
+    List<Tarefa> listarTodas();
+}
---
+++ src/main/java/Swing/model/Tarefa.java
@@ -0,0 +1,23 @@
+package Swing.model;
+
+public class Tarefa {
+    private int id;
+    private String descricao;
+
+    public Tarefa(int id, String descricao){
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
